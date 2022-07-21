0404cbbf260b823bf8ebb7e0bddd12f0120ed42c
abaza738 <maher.abaza@protonmail.com>
2022-07-21 23:18:07 +0300

    Fix info command.

--- src/commands/info.cmd.ts
+++ src/commands/info.cmd.ts
@@ -2,40 +2,46 @@ import { CommandInteraction } from "discord.js";
 import { Discord, Slash } from "discordx";
 import moment from "moment";
 import { embedFactory } from "../common/utils.js";
-// @ts-ignore
-// import * as packageJson from "../../package.json";
+import fs from 'fs';
 
 @Discord()
 export abstract class Info {
+  private readonly packageLocation: string = process.env.npm_package_json!;
+  private packageJson: Record<string, any> = {};
+
+  constructor() {
+    if (typeof this.packageLocation !== 'string') {
+      return;
+    }
+    const fileByteArray = fs.readFileSync(this.packageLocation, 'utf8');
+    this.packageJson = JSON.parse(fileByteArray);
+  }
+
   @Slash('info', { description: 'General information about the bot' })
   async info(interaction: CommandInteraction) {
 
-    // Temporary solution until I figure out what's wrong with importing package.json
-    const pkg = {
-      name: "Jo Bot",
-      description: "DiscordTS Bot for Jordanian Aviators server.",
-      repository: {
-        url: "https://github.com/abaza738/jo-ts"
-      },
-      author: "Maher Abaza",
-      version: "1.1.0"
-    };
+    if (!this.packageJson.name || !this.packageJson.description || !this.packageJson.version) {
+      await interaction.reply('Oepsie! Something went wrong.');
+
+      setTimeout(() => interaction.deleteReply(), 10e3);
+      return;
+    }
 
     const embed = embedFactory({
-      title: `General information about ${pkg.name}`,
+      title: `General information about ${this.packageJson.name}`,
       interaction: interaction,
-      description: `${pkg.description}`
+      description: `${this.packageJson.description}`
     })
     .addFields(
       { name: `Servers`, value: `${interaction.client.guilds.cache.size}`, inline: true},
       { name: `Up Time`, value: `${moment.duration(interaction.client.uptime, "milliseconds").humanize()}`, inline: true},
       { name: `Members`, value: `${interaction.client.guilds.cache.reduce((acc: number, value) => acc + value.memberCount, 0)}`, inline: true},
       { name: `Invite`, value: `[Invite Jo Bot to your server!](https://ptb.discord.com/api/oauth2/authorize?client_id=781557275235188766&permissions=8&scope=bot%20applications.commands)`, inline: true},
-      { name: `Contribute`, value: `Submit issues and contribute to the bot's developmen on [GitHub](${pkg.repository.url})!`},
-      { name: `Version`, value: `${pkg.version}`},
+      { name: `Contribute`, value: `Submit issues and contribute to the bot's developmen on [GitHub](${this.packageJson.repository.url})!`},
+      { name: `Version`, value: `${this.packageJson.version}`},
     )
-    .setFooter({ text: `Made by ${pkg.author}`, iconURL: `https://webstockreview.net/images/github-icon-png-8.png` });
+    .setFooter({ text: `Made by ${this.packageJson.author}`, iconURL: `https://webstockreview.net/images/github-icon-png-8.png` });
 
-    interaction.reply({ embeds: [embed] });
+    await interaction.reply({ embeds: [embed] });
   }
 }
