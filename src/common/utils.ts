90a035e40f5a3c5b5a923e5b22b81558dbe4f903
abaza738 <maher.abaza@protonmail.com>
2022-10-31 22:31:24 +0300

    Listen to message deletion events.

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -155,3 +155,16 @@ export function toRad(value: number): number {
 export function toDeg(value: number): number {
   return value * 180 / Math.PI;
 }
+
+/**
+ * Async function to sleep for `ms` amound of milliseconds.
+ * @param ms - time to sleep in milliseconds
+ * @returns `Promise<void>` after the sleep time has passed.
+ */
+export async function sleep(ms: number): Promise<void> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, ms);
+  });
+}
